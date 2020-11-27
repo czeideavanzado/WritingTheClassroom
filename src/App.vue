c9fe4c8d112882f59c08fcf329c0c85aa7d234b8
Czeide Avanzado <czeideavanzado@gmail.com>
2020-11-27 15:55:51 +0800

    Fix eslint warnings

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,16 @@
   <v-app id="everything">
     <v-app-bar app id="navbar">
       <div class="d-flex align-center">
-        <LogoOnly>
-        </LogoOnly>
+        <LogoOnly> </LogoOnly>
         <v-img
           alt="Writing The Classroom Logo"
           class="shrink mr-2"
           contain
-          src="..\src\assets\logo.png"
+          src="../src/assets/logo.png"
           transition="scale-transition"
           width="40"
         />
-        <div style="height:100%; font-size: 100%"> Writing The Classroom</div>
-        
-       
+        <div style="height:100%; font-size: 100%">Writing The Classroom</div>
       </div>
 
       <v-spacer></v-spacer>
@@ -25,13 +22,11 @@
         color="white"
       >
         <span class="mr-2">REGISTER</span>
-        
       </v-btn>
     </v-app-bar>
 
     <v-main>
       <HelloWorld />
-    
     </v-main>
   </v-app>
 </template>
@@ -43,7 +38,7 @@ export default {
   name: "App",
 
   components: {
-    HelloWorld,
+    HelloWorld
   },
 
   data: () => ({
@@ -53,26 +48,26 @@ export default {
 </script>
 
 <style scoped>
-#navbar{
-  background-color:rgba(0,0,0,0.6);
+#navbar {
+  background-color: rgba(0, 0, 0, 0.6);
   color: white;
 }
 
- #everything {
-	background: linear-gradient(-45deg, #261ac1, #2f3dbc, #4121b7, #4121b7);
-	background-size: 400% 400%;
-	animation: gradient 15s ease infinite;
+#everything {
+  background: linear-gradient(-45deg, #261ac1, #2f3dbc, #4121b7, #4121b7);
+  background-size: 400% 400%;
+  animation: gradient 15s ease infinite;
 }
 
 @keyframes gradient {
-	0% {
-		background-position: 0% 50%;
-	}
-	50% {
-		background-position: 100% 50%;
-	}
-	100% {
-		background-position: 0% 50%;
-	}
+  0% {
+    background-position: 0% 50%;
+  }
+  50% {
+    background-position: 100% 50%;
+  }
+  100% {
+    background-position: 0% 50%;
+  }
 }
 </style>
