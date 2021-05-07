f0e77e677f2ad808d44149871546c24a3189c53f
Czeide Avanzado <czeideavanzado@gmail.com>
2021-05-08 05:23:08 +0800

    Add views for speakers, organizers, schedule, repository, and registration

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app id="everything">
     <v-app-bar class="white--text" color="rgba(0, 0, 0, 0.6)" app>
-      <v-app-bar-nav-icon v-if="false" color="white" @click="drawer = !drawer">
+      <v-app-bar-nav-icon color="white" @click="drawer = !drawer">
       </v-app-bar-nav-icon>
 
       <div>
