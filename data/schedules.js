95cdfcb4ba71f26000a4716387941856e51e798e
Czeide Avanzado <czeideavanzado@gmail.com>
2021-05-16 22:07:38 +0800

    Move schedules data to a separate file

---
+++ data/schedules.js
@@ -0,0 +1,104 @@
+"use strict";
+
+module.exports = [
+  {
+    date: "Jun 17, 2021 (Thursday)",
+    morning: [
+      {
+        text: "Opening Ceremonies"
+      },
+      {
+        text: "Keynote Lecture by Sir Ricky Lee",
+        image: require("../src/assets/speakers/Ricky Lee.jpg"),
+        imageTitle: "Ricky Lee"
+      },
+      {
+        text: "Panel 1: Teaching Poetry/Pagtuturo ng Tula",
+        lectures: [
+          {
+            title: "Ang Naituturo ng Tula",
+            instructor: "Mesándel Virtusio Arguelles",
+            image: require("../src/assets/speakers/Ayer Arguelles.jpg"),
+            description:
+              "Sa master class na ito, sisikaping maituro, i.e., matukoy, maihayag, at maipaunawa kung ano ang (maaaring) naituturo ng tula. Bagamat may guro na nagtuturo (pagbasa at pagsulat) ng tula, higit ang diin sa tula mismo bilang siyang gabay/guro; sa pagkilala/pangangailangan sa tula bilang tagpuan at pagtatagpo na labas/lumalabas sa silid-aralan (kung/habang nasa silid-aralan)."
+          },
+          {
+            title: "Doing Philosophy through Poetry",
+            instructor: "Noelle Leslie dela Cruz",
+            image: require("../src/assets/speakers/Noelle Leslie dela Cruz.jpg"),
+            description:
+              "The lecture explores the relations between poetry and philosophy, providing an overview of selected issues drawn from philosophy of literature, philosophy of poetry, and philosophy of language, such as truth in/and poetry, metaphorical meaning, and lyric philosophy. Poetic works are used to shed light on philosophical problems. It also discusses modern and contemporary poets whose works are inclined toward philosophy. It culminates in a couple of workshops on (1) reading philosophy in poetry and (2) doing philosophy through poetry."
+          },
+          {
+            title:
+              "Poetry, Rap, and Spoken Word - Giving Voice to the Voiceless",
+            instructor: "P.J. Martin",
+            image: require("../src/assets/speakers/PJ Martin.jpg"),
+            description:
+              "In 1979, “Rapper’s Delight” by The Sugarhill Gang became the first rap single to become a Top 40 hit. Thirty-nine years later, rapper Kendrick Lamar’s album DAMN. won the Pulitzer Prize—the first non-classical, non-jazz record to receive this distinction. The genre emerged out of inner-city, African-American neighborhoods, but its origins are undeniably rooted in poetry. So how—and most importantly, why—did this form of literature, usually associated with highbrow society, become the preferred medium for historically un- and under-educated, marginalized communities to tell their story? What can we glean from the accessibility of verse that could help us enrich and empower our own students, especially those who find themselves academically deficient and/or disenfranchised? This workshop will include a brief background in the origins of “poems set to a beat” and spoken word poetry, with an emphasis on seminal works that have been catalysts for social change. Then we will share practical classroom activities with the objective of piquing students’ interest in poetry (with popular rap music and spoken word as an entrée), honing the skillset required in the appreciation and creation of poetry, and encouraging self-expression."
+          }
+        ]
+      }
+    ],
+    afternoon: [
+      {
+        text: "Panel 2: Teaching Fiction/Pagtuturo ng Kuwento",
+        lectures: [
+          {
+            title: "Pagkwentuhan Ang Pagkukwento",
+            subtitle:
+              "Ang Pagtuturo at Pag-aaral ng Kwento sa Panahon ng Pad Paper hanggang Wattpad",
+            instructor: "Eros Atalia",
+            image: require("../src/assets/speakers/Eros Atalia.jpg"),
+            description:
+              "Isa sa pinakamasiglang industriya at disiplina sa ngayon, hatid na rin ng iba’t ibang media platform (wattpad, facebook, twitter) ang malikhaing pagsulat partikular ang novella, maikling kwento at dagli. Gayundin, sa pagsasama ng Malikhaing Pagsulat/Creative Writing sa K+12 Curriculum. Malakas ang demand ng mga publishing company para sa mga bagong akda lalong-lalo na sa classroom dahil sa localization at indigenization ng mga akda.  Naglalayon ang diskursong ito na magabayan ang mga paparating na manunulat (teacher at estudyante) mula sa malikhaing proseso at aktwal na pagsusulat hanggang sa maibahagi ito sa iba’t ibang paraan (libro, e-book, wattpad, littweet, pelikula, etc) lalong-lalo na kung paano maituturo ang pagpapasulat na magagamit sa loob at labas ng classroom."
+          },
+          {
+            title: "Characters & their Worlds",
+            instructor: "Clarissa Militante",
+            image: require("../src/assets/speakers/Clarissa Militante.jpg"),
+            description:
+              "Readers enter the world of story through the characters who serve as mirrors of human behavior and psychology. But another critical role of characters in fiction is to give us a world that can parallel, reject, or re-imagine our own. This masterclass shall focus on the close & critical reading of works of fiction while paying attention to techniques in characterization and world building so that in turn teachers can use or pass on these skills that can help students increase/deepen their understanding of humanity and the contemporary world."
+          },
+          {
+            title: "Setting as Antagonist/Lunan bilang Kontrabida",
+            instructor: "John Iremil Teodoro",
+            image: require("../src/assets/speakers/John Iremil Teodoro.jpg"),
+            description:
+              "Writing short fiction is creating a temporary world that the reader can visit in one sitting. Ang setting o lunan bilang isa sa mga elemento ng isang kuwento ay nagsisilbi bilang background, atmosphere, metaphor, at environmental force. This master class underscores setting as an antagonist or a force shaping the plot and the characters. Magkakaroon ng dalawang bahagi ang klaseng ito. Una, pagbabasa at talakayan ng dalawang maikling kuwento ni Leoncio P. Deriada na “The Week of the Whales” at “Ang Kamatayon sang Isa ka Kalye” concentrating on the setting as fictional element at kung paano nahuhubog ang plot at ang mga karakter sa dalawang kuwento. Second, a workshop where participants will craft an idea of a short story they are going to write where setting is a strong environmental force. Hindi lamang tungkol sa pagsusulat ng katha ang sesyon na ito kundi tungkol din sa pagtuturo kung paano magbasa at magsulat ng short story."
+          }
+        ]
+      },
+      {
+        text: "Panel 3: Teaching Creative Nonfiction/Pagtuturo ng Sanaysay",
+        lectures: [
+          {
+            title:
+              "What’s Sex Got to Do With It?: Exploring Queer Identity Politics and Creative Nonfiction",
+            instructor: "Ronald Baytan",
+            image: require("../src/assets/speakers/Ronald Baytan.jpg"),
+            description:
+              "This class explores how the writing of creative nonfiction (CNF), like all other genres of literature, is a highly political act, and an empowering one, too.  The “self” that nonfiction writers create through various platforms or media is inevitably gendered and “sexed.”  As teachers, we need to ask how we can use our CNF class towards a critical examination and revaluation of hegemonic and heteronormative practices that have plagued the world for so long. How can we fuse the development or sharpening of creative writing and critical thinking skills with the ethics of life writing?  How can we use creative writing as a methodological tool and/or approach to broaden our students’ understanding of sex, gender, and sexuality?  Using sample texts by LGBTQIA and women writers, this module will teach students/teachers how to “queer” their class and how to design tasks that will deepen students’ consciousness of their subject-position as gendered/sexual beings."
+          },
+          {
+            title:
+              "Maituturo ba talaga ang Malikhaing Pagsulat?: Ang Sanaysay Bílang Isang Anyo ng Pagtatapat",
+            instructor: "Genaro Gojo Cruz",
+            image: require("../src/assets/speakers/Genaro Gojo Cruz.jpg"),
+            description:
+              "Ang sanaysay ang pinakamabisang anyo ng panitikan upang magtapat ang isang manunulat.  Sa sanaysay ang manunulat ang mismong nagsasalita, walang pagtatago o pagtatakip.  Bílang isang anyo, malawak o iba’t iba ang maibibigay na halimbawa ng sanaysay.<br /><br />" +
+              "Ang lekturang ito ay magbibigay-tuon sa sanaysay bílang isang anyo ng kumpisal o pagtatapat (confession). Tatalakayin ang ilan sa mga halimbawang teksto na may ganitong intensiyon o layon, kung paano ito wastong maipababasa at kritikal na maituturo/maipapasuri sa mga mag-aaral sa Senior High School na di lámang nakatuon sa kung anong mga halagahan (values) ang makukuha o matutuhan dito.<br /><br />" +
+              "Layon din ng lektura na makapagbigay ng ilang mga napapanahon at makabuluhang gawain sa mga guro; kung paanong maging malikhaing guro sa pagtuturo ng malikhaing pagsulat; at kung paano magagamit ang sanaysay bílang behikulo ng mga mag-aaral sa pagtatapat sa kanilang sulatin upang makapagparanas/makapagpadama ng kanilang katangi-tangi/makukulay na karanasan sa iba, bílang panghuling kahingian sa creative nonfiction, na isang asignatura ngayon sa Senior High School."
+          },
+          {
+            title: "How to Live Ahead of Your Life: The Art of Memoir Writing",
+            instructor: "Dinah T. Roma",
+            image: require("../src/assets/speakers/Dinah Roma.jpg"),
+            description:
+              "The memoir is one of the many types of creative nonfiction genre (CNF) that appeals to young, beginning writers. Some of the reasons for this is that it provides a chance to reflect on an aspect of one’s life, craft it into a powerful and inspiring narrative, and articulate its value for growth. This lecture/workshop on memoir writing is divided into two parts. The first part will consider the more effective ways of writing a memoir by looking at the philosophy behind its writing, the concept of time and narrative framing, and the “bigger idea” behind a memoir. This will largely be done through the close reading of excerpts from published creative nonfiction books. In the second part of the workshop, participants will be asked to identify their own stories, outline their narratives, and write a one-page essay that integrate the lessons from the first part. The workshop will conclude with a discussion synthesizing how the participants’ individual writings may be improved for completion and future publishing plans."
+          }
+        ]
+      }
+    ]
+  }
+];
